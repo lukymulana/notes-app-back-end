ed6def61952e4dc1e4cb8e42181ec5f7e159bcd2
lukymulana <lukymulana@gmail.com>
2021-08-23 11:03:32 +0700

    user registration implemented

---
+++ src/api/users/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+
+const UsersHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'users',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const usersHandler = new UsersHandler(service, validator);
+    server.route(routes(usersHandler));
+  },
+};
---
+++ src/validator/users/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+const InvarianError = require('../../exceptions/InvariantError');
+const { UserPayloadSchema } = require('./schema');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validationResult = UserPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvarianError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
